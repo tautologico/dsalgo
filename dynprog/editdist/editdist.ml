dbccae259a91488322e75dd349debc0106e05be6
Andrei Formiga <andrei.formiga@gmail.com>
2010-09-12 20:27:44 -0300

    - Finished implementation of edit distance - Added description of edit distance problem

--- dynprog/editdist/editdist.ml
+++ dynprog/editdist/editdist.ml
@@ -2,19 +2,32 @@
 
   editdist.ml
   Edit distance calculation using Dynamic Programming
-  (as suggested by an exercise in Chapter 15 of CLRS, 3rd ed.)
+  (as suggested by an assigment from the MIT Introduction to Algorithms course)
+  http://bit.ly/bN7vUR
 
   Andrei de A. Formiga, 2010-09-11
 
 *)
 
+(*
+
+  See editdist1.org for a problem description and solution guide. 
+  
+*)
+
+
 type action = Left | Right | Replace | Insert | Delete
+
+let action_cost = function
+  | Left | Right -> 0
+  | Replace -> 4
+  | Insert -> 3
+  | Delete -> 2
+
 
 
 (** Utility function *)
 let min3 a b c = min (min a b) c
-
-(* TODO: Describe solution idea and recursive equation *)
 
 
 (* Recursive solution *)
@@ -31,7 +44,6 @@ let rec editdist_rec x i y j =
 (*
 
 Table-based bottom-up solution. 
-
 
 *)
 let init_matrix m n = 
@@ -61,12 +73,26 @@ let editdist x y =
   let d = editdist_mat x y in
   d.(0).(0)
 
+(** Rebuild the sequence of transformations from the matrix *)
+let editdist_ops x y = 
+  let m = String.length x in
+  let n = String.length y in
+  let d = editdist_mat x y in
+  let rec loop i j = 
+    if i >= m && j >= n then []
+    else if i >= m then Insert :: loop i (j+1) 
+    else if j >= n then Delete :: loop (i+1) j
+    else if x.[i] == y.[j] then Right :: loop (i+1) (j+1)
+    else if d.(i).(j) == (d.(i+1).(j) + 2) then Delete :: loop (i+1) j
+    else if d.(i).(j) == (d.(i).(j+1) + 3) then Insert :: loop i (j+1)
+    else Replace :: loop (i+1) (j+1) in
+  loop 0 0
+
+(* TODO: Build table displaying operations *)
+
 
 (* Tests *)
 let x1 = "C is a relatively \"low level\" language."
 let y1 = "Java is an object-oriented language."
 let sol1 = 73
-
-
-      
 
