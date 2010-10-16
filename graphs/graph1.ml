045d2f3b876c5324b0df0974349555ec21a4f7f7
Andrei Formiga <andrei.formiga@gmail.com>
2010-10-16 02:06:46 -0300

    Added functions on graph implementation.

--- graphs/graph1.ml
+++ graphs/graph1.ml
@@ -26,6 +26,7 @@ type graph =
     }
 
 
+(* the undirected graph shown in Figure 5.4, page 151 *)
 let g1 = { nvertices = 5; nedges = 7; directed = false;
            vertices = [ ([(2, 1); (5, 1)], 2);                   (* v1 *)
                         ([(1, 1); (5, 1); (3, 1); (4, 1)], 4);   (* v2 *)
@@ -33,3 +34,17 @@ let g1 = { nvertices = 5; nedges = 7; directed = false;
                         ([(2, 1); (5, 1); (3, 1)], 3);           (* v4 *)
                         ([(4, 1); (1, 1); (2, 1)], 3) ]          (* v5 *)
          }
+
+
+(* functions *)
+let initialize_graph directed = 
+  { nvertices = 0; nedges = 0; directed = directed; vertices = [] }
+
+let read_graph directed = 
+  { nvertices = 0; nedges = 0; directed = directed; vertices = [] } (* TODO *)
+
+let insert_edge g x y directed =                                    (* TODO *)
+  ()
+
+
+
